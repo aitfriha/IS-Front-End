34e4c73cd07cb04592adcd6984e7200e26d00331
zaitfriha <zaid.aitfriha@techniu.org>
2020-12-31 11:21:45 +0100

    add commercial action to the menu

--- app/containers/App/Application.js
+++ app/containers/App/Application.js
@@ -35,6 +35,7 @@ import {
   Operations,
   OperationsAssignment,
   CommercialOperation,
+  CommercialAction,
   AddCommercialOperation,
   FinancialCompany,
   AddFinancialCompany,
@@ -260,6 +261,11 @@ function Application(props) {
           exact
           path="/app/gestion-commercial/Commercial-Operations"
           component={CommercialOperation}
+        />
+        <Route
+            exact
+            path="/app/commercial-action"
+            component={CommercialAction}
         />
         <Route
           exact
