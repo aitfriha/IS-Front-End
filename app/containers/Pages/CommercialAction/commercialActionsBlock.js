b8584d0db7c9e888bcadf88d0a43475f5bc7b722
aymenst <aymenst01@gmail.com>
2021-04-08 16:25:46 +0100

    Financial Model Update

--- app/containers/Pages/CommercialAction/commercialActionsBlock.js
+++ app/containers/Pages/CommercialAction/commercialActionsBlock.js
@@ -84,7 +84,6 @@ class CommercialActionsBlock extends React.Component {
       console.log(result);
       this.setState({ assignments: result.data });
     });
-
     interact('.resize-drag')
       .draggable({
         // enable autoScroll
@@ -125,6 +124,19 @@ class CommercialActionsBlock extends React.Component {
         },
       });
   }
+
+  dragMoveListener = (event) => {
+    const { target } = event;
+    // keep the dragged position in the data-x/data-y attributes
+    const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+    const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+    // translate the element
+    target.style.webkitTransform = 'translate(' + x + 'px, ' + y + 'px)';
+    target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  };
 
     handleChange = (ev) => {
       this.setState({ [ev.target.name]: ev.target.value });
